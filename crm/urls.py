89062859fba0b78b4610e44031195be5bb2a556f
felipex <felipexcavalcante@gmail.com>
2026-04-03 11:42:36 -0300

    feat: Create initial templates and links

--- crm/urls.py
+++ crm/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from crm.views import CustomerListView, CustomerCreateView
+from crm.views import IndexView, CustomerListView, CustomerCreateView, CustomerUpdateView
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('customers/', CustomerListView.as_view(), name='customer_list'),
     path('customers/create/', CustomerCreateView.as_view(), name='customer_create'),
+    path('customers/<uuid:pk>/update/', CustomerUpdateView.as_view(), name='customer_update'),
 ]
 
--- crm/views.py
+++ crm/views.py
@@ -1,10 +1,12 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView
-from django.views.generic.edit import CreateView
-
+from django.views.generic import ListView, TemplateView
+from django.views.generic.edit import CreateView, UpdateView
 from crm.forms import CustomerForm
 from crm.models import Customer
 
+
+class IndexView(TemplateView):
+    template_name = 'crm/index.html'
 
 class CustomerListView(ListView):
     model = Customer
@@ -23,3 +25,14 @@ class CustomerCreateView(CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
+
+
+class CustomerUpdateView(UpdateView):
+    model = Customer
+    form_class = CustomerForm
+    template_name = 'crm/customers/update_customer.html'
+    success_url = reverse_lazy('customer_list')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form) 
